3921a1fe64c055bc9ff928c8daa6cdf23b7c9e8b
NathanCrazy <97754720+NathanCrazy@users.noreply.github.com>
2023-08-08 12:31:43 -0400

    Hate.

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import cellmachine
+import cellmachine as cm
 import sys
+
 fileCode = sys.argv[1]
+vault = cm.import_level(fileCode)
+vaultArray = vault.cells.values()
+print(vaultArray[0])
+
 vaultNotation = sys.argv[2]
+sections = vaultNotation.split(";")
+if sections[0] != "V4":
+    raise Exception("How dare you send a notation without the Holy V4.")
+ticks = sections[3].split(" ")
+for tick in ticks:
+    cells = tick.split(".")
+
+print("test")
