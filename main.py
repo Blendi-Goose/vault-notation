69561388c807f9ac5938ec29cceb753dc8dc9416
NathanCrazy <97754720+NathanCrazy@users.noreply.github.com>
2023-08-08 12:46:26 -0400

    hr

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import cellmachine as cm
 import sys
 
-fileCode = sys.argv[0]
+fileCode = sys.argv[1]
 print(fileCode)
 vault = cm.import_level(fileCode)
 vaultArray = vault.cells.values()
 print(vaultArray[0])
 
-vaultNotation = sys.argv[1]
+vaultNotation = sys.argv[2]
 sections = vaultNotation.split(";")
 if sections[0] != "V4":
-    raise Exception("How dare you send a notation without the Holy V4.")
+    print("How dare you send a notation without the Holy V4.")
 ticks = sections[3].split(" ")
 for tick in ticks:
     cells = tick.split(".")
