e5d2db10f2f9152924b92a8db14ef06f12c3852b
NathanCrazy <97754720+NathanCrazy@users.noreply.github.com>
2023-08-09 10:36:20 -0400

    ner fix

--- ner.py
+++ ner.py
@@ -21,7 +21,7 @@ def ner(facing, thrust, pos, length, vault):
         pos = funcs.relativeMove(facing, 1, pos, 1, vault)
         cm.cells.Immobile(vault, tuple(pos))
     # Right push and gen
-    pos = funcs.relativeMove(facing, 3, pos, 1, vault)
+    pos = funcs.relativeMove(facing, 1, pos, 1, vault)
     for i in range(thrust):
         pos = funcs.relativeMove(facing, 0, pos, 1, vault)
         cm.cells.Push(vault, tuple(pos))
@@ -46,7 +46,7 @@ def ner(facing, thrust, pos, length, vault):
     corner2 = pos
     pos = funcs.relativeMove(facing, 3, pos, 1, vault)
     for i in range(thrust):
-        pos = funcs.relativeMove(facing, 3, pos, 1, vault)
+        pos = funcs.relativeMove(facing, 2, pos, 1, vault)
         cm.cells.Trash(vault, tuple(pos))
     print(cm.levelstring.v3.export_level(vault))
     return [corner1, corner2]
