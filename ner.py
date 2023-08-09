c4f35839ba86789aef70c316a9202d23cbdd4645
NathanCrazy <97754720+NathanCrazy@users.noreply.github.com>
2023-08-09 04:15:37 -0400

    tea

--- ner.py
+++ ner.py
@@ -28,7 +28,7 @@ def ner(facing, thrust, pos, length, vault):
         cm.cells.Generator(vault, tuple(pos), 1)
         pos = funcs.relativeMove(facing, 1, pos, 1, vault)
     #Final Array
-    pos = funcs.relativeMove(facing, 3, pos, 2, vault)
+    pos = funcs.relativeMove(facing, 3, pos, 1, vault)
     for i in range(thrust):
         for j in range(length):
             pos = funcs.relativeMove(facing, 3, pos, 1, vault)
@@ -36,10 +36,13 @@ def ner(facing, thrust, pos, length, vault):
         pos = funcs.relativeMove(facing, 1, pos, length, vault)
         pos = funcs.relativeMove(facing, 2, pos, 1, vault)
     pos = funcs.relativeMove(facing, 0, pos, 1, vault)
+    pos = funcs.relativeMove(facing, 3, pos, 1, vault)
     cm.cells.Generator(vault, tuple(pos), 1)
     corner1 = pos
     pos = funcs.relativeMove(facing, 0, pos, thrust, vault)
     pos = funcs.relativeMove(facing, 3, pos, length, vault)
+    pos = funcs.relativeMove(facing, 1, pos, 1, vault)
+    pos = funcs.relativeMove(facing, 2, pos, 1, vault)
     cm.cells.Generator(vault, tuple(pos), 1)
     corner2 = pos
     print(cm.levelstring.v3.export_level(vault))
