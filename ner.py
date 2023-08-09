d7a3fe40a1e396afed2abb2d25d1fc3c94da317a
NathanCrazy <97754720+NathanCrazy@users.noreply.github.com>
2023-08-09 04:09:49 -0400

    ner working?

--- ner.py
+++ ner.py
@@ -27,10 +27,19 @@ def ner(facing, thrust, pos, length, vault):
         pos = funcs.relativeMove(facing, 3, pos, 1, vault)
         cm.cells.Generator(vault, tuple(pos), 1)
         pos = funcs.relativeMove(facing, 1, pos, 1, vault)
+    #Final Array
+    pos = funcs.relativeMove(facing, 1, pos, 2, vault)
+    for i in range(thrust):
+        for j in range(length):
+            pos = funcs.relativeMove(facing, 1, pos, 1, vault)
+            cm.cells.Push(vault, tuple(pos))
+        pos = funcs.relativeMove(facing, 3, pos, length, vault)
+        pos = funcs.relativeMove(facing, 2, pos, 1, vault)
+    pos = funcs.relativeMove(facing, 0, pos, 1, vault)
     cm.cells.Generator(vault, tuple(pos), 1)
     corner1 = pos
-    #pos = funcs.relativeMove(facing, 0, pos, thrust, vault)
-    #pos = funcs.relativeMove(facing, 1, pos, length, vault)
+    pos = funcs.relativeMove(facing, 0, pos, thrust, vault)
+    pos = funcs.relativeMove(facing, 3, pos, length, vault)
     cm.cells.Generator(vault, tuple(pos), 1)
     corner2 = pos
     print(cm.levelstring.v3.export_level(vault))
