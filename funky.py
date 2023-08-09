f107a24d064409916f4ed21bffd26a599c37b1e1
NathanCrazy <97754720+NathanCrazy@users.noreply.github.com>
2023-08-08 20:24:39 -0400

    okay ig i need more than one file, damn

---
+++ funky.py
@@ -0,0 +1,31 @@
+import cellmachine as cm
+
+def fuse(length, firstPush, vault, facing):
+    pos = firstPush
+    for i in range(length):
+        cm.cells.Push(vault, pos)
+        pos = relativeMove(facing, 2, pos, 1, vault)
+        cm.cells.Immobile(vault, pos)
+        pos = relativeMove(facing, 0, pos, 2, vault)
+        cm.cells.Immobile(vault, pos)
+        pos = relativeMove(facing, 3, pos, 1, vault)
+        pos = relativeMove(facing, 2, pos, 1, vault)
+        print(vault.export_level)
+    return
+
+def relativeMove(facing, direction, position, magnitude, vault):
+    # Facing starts at right, direction starts at forward. Both go clockwise.
+    newDirection = facing + direction
+    if newDirection % 2 == 0:
+        if newDirection == 0:
+            newposition = [position[0] + magnitude, position[1]]
+        else:
+            newposition = [position[0] - magnitude, position[1]]
+    else:
+        if newDirection == 3:
+            newposition = [position[0], position[1] + magnitude]
+        else:
+            newposition = [position[0], position[1] - magnitude]
+    if vault.outside_bounds(newposition):
+        raise Exception("Area too small for proper ner to be made, please expand.")
+    return newposition
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cellmachine as cm
 import sys
+import funky as funcs
 
 fileCode = sys.argv[1]
 vault = cm.import_level(fileCode)
@@ -36,36 +37,6 @@ if corner1[1] != corner2[1]:
 print("0 is right, goes counterclockwise: " + str(facing))
 if onegapontop == 2:
     raise Exception("\033[1;31mNow hold your horses, buckaroo. Only one gaps allowed in this house.\033[0m\n")
-fuse(20, corner1)
-# FUSE FRAMEWORK
-# Will write over cells in the way of the fuse! Manual fixing may be necessary.
 
-def fuse(length, firstPush):
-    pos = firstPush
-    for i in range(length):
-        cm.cells.Push(vault, pos)
-        pos = relativeMove(2, pos, 1)
-        cm.cells.Immobile(vault, pos)
-        pos = relativeMove(0, pos, 2)
-        cm.cells.Immobile(vault, pos)
-        pos = relativeMove(3, pos, 1)
-        pos = relativeMove(2, pos, 1)
-        print(vault.export_level)
-    return
+funcs.fuse(20, corner1, vault, facing)
 
-def relativeMove(direction, position, magnitude):
-    # Facing starts at right, direction starts at forward. Both go clockwise.
-    newDirection = facing + direction
-    if newDirection % 2 == 0:
-        if newDirection == 0:
-            newposition = [position[0] + magnitude, position[1]]
-        else:
-            newposition = [position[0] - magnitude, position[1]]
-    else:
-        if newDirection == 3:
-            newposition = [position[0], position[1] + magnitude]
-        else:
-            newposition = [position[0], position[1] - magnitude]
-    if vault.outside_bounds(newposition):
-        raise Exception("Area too small for proper ner to be made, please expand.")
-    return newposition
